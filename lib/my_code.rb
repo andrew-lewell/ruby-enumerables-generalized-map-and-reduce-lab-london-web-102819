d041180d532e08c03af460e4de2a79e6754c12c1
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-14 16:10:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def reduce(array, starting_point = 0)
     total = starting_point
     counter = 0 
   else
-    total = array[0]]
+    total = array[0]
     counter = 1
   while counter < array.length do 
     total = yield(total, (array[counter]))
